79d45c493cb83eb7e855cae9e5ab87600abb6b67
Ken Reed <kenakioreed@gmail.com>
2019-11-13 21:19:33 -0500

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,202 @@
+use demodesktopgraphics::glutin;
+use demodesktopgraphics::GlSys;
+use demodesktopgraphics::Vertex;
+use axgeom::Vec2;
+use axgeom::vec2;
+use fps_counter::FPSCounter;
+use glutin::event::WindowEvent;
+use glutin::event::ElementState;
+use glutin::event::VirtualKeyCode;
+use glutin::event::Event;
+use glutin::event_loop::ControlFlow;
+
+fn main() {
+    rayon::ThreadPoolBuilder::new().num_threads(num_cpus::get_physical()).build_global().unwrap();
+     
+
+    let mut events_loop = glutin::event_loop::EventLoop::new();
+
+
+    let mut botsys=pathfind::game::Game::new();
+
+    let mut glsys=GlSys::new(&events_loop);
+    
+
+    let (startx,starty)=glsys.get_dim();
+    
+    //let mut border=compute_border(game_response.new_game_world.unwrap().0,[startx as f32,starty as f32]);
+    let border=axgeom::Rect::new(-200.0,200.0,-200.0,200.0);
+    let radius=10.0;
+
+    
+
+    let mut bot_buffer=demodesktopgraphics::Buffer::create_vbo(0);
+
+    let mut wall_buffer=demodesktopgraphics::Buffer::create_vbo(0);
+        
+
+    struct Ba{
+        pos:Vec2<f32>,
+        id:u64
+    }
+
+    let mut mousepos=vec2(0.0,0.0);
+    let mut mouse_active=false;
+    let mut poses:Vec<Ba>=Vec::new(); 
+    
+    let mut fps=FPSCounter::new();
+
+    let mut last_time:Option<std::time::Instant>=None;
+
+    events_loop.run(move |event,_,control_flow| {
+        match event {
+            Event::WindowEvent{ event, .. } => match event {
+                WindowEvent::KeyboardInput{input,..}=>{       
+                    match input.virtual_keycode{
+                        Some(VirtualKeyCode::Escape)=>{
+                            *control_flow=ControlFlow::Exit;
+                        },
+                        _=>{}
+                    }
+                },
+                WindowEvent::CloseRequested => {*control_flow=ControlFlow::Exit;},
+                WindowEvent::Resized(_logical_size) => {
+                    
+                },
+                WindowEvent::CursorMoved{modifiers:_,device_id:_,position:logical_position} => {
+                    let glutin::dpi::LogicalPosition{x,y}=logical_position;
+                    mousepos=vec2(x as f32,y as f32);
+                },
+                WindowEvent::MouseInput{modifiers:_,device_id:_,state,button}=>{
+                    if button==glutin::event::MouseButton::Left{
+                        match state{
+                            glutin::event::ElementState::Pressed=>{  
+                                mouse_active=true;  
+                                
+                            }
+                            glutin::event::ElementState::Released=>{
+                                mouse_active=false;
+                            }
+                        }
+                    }
+                },
+                WindowEvent::Touch(touch)=>{
+                    let glutin::dpi::LogicalPosition{x,y}=touch.location;
+                    //let x=(x*0.84) as f32; //TODO why needed????
+                    //let y=(y*0.84) as f32; 
+                    let x=x as f32;
+                    let y=y as f32;
+
+                    match touch.phase{
+                        glutin::event::TouchPhase::Started=>{
+
+                            let mut found=false;
+                            for i in poses.iter(){
+                                if i.id == touch.id{
+                                    //panic!("There is a problem!");
+                                    found=true;
+                                    break;
+                                }
+                            }
+                            if found==false{
+                                poses.push(Ba{id:touch.id,pos:vec2(x,y)});
+                            }
+                        },
+                        glutin::event::TouchPhase::Ended | glutin::event::TouchPhase::Cancelled=>{
+                            //poses.clear();
+                            poses.retain(|a|a.id!=touch.id);
+                        },
+                        glutin::event::TouchPhase::Moved=>{
+                            let mut ok=false;
+                            for k in poses.iter_mut(){
+                                if k.id==touch.id{
+                                    k.pos=vec2(x,y);
+                                    ok=true;
+                                    break;
+                                }
+                            }
+                            
+                            if ok ==false{
+                                panic!("Didnt find touch");
+                            }
+                          
+                        }
+                    }
+                },
+                _=>{}
+            },
+            Event::EventsCleared=>{
+                let do_run = match last_time{
+                    Some(last_time)=>{
+                        if last_time.elapsed().as_millis()>=16{
+                            true
+                        }else{
+                            false
+                        }
+                    },
+                    None=>{
+                        true
+                    }
+                };
+
+                if do_run{
+
+                    let mut va:Vec<Vec2<f32>>=poses.iter().map(|a|a.pos).collect();
+                    if mouse_active{
+                        let mouseposx=mousepos.x-(startx as f32/2.0);
+                        let mouseposy=mousepos.y-(starty as f32/2.0);
+                    
+                        let ((x1,x2),(y1,y2))=border.get();
+                        let w=x2-x1;
+                        let h=y2-y1;
+
+                        let mouseposx=mouseposx*(w/startx as f32);
+                        let mouseposy=mouseposy*(h/starty as f32);
+                       
+                        va.push(vec2(mouseposx,mouseposy));
+                    }
+
+
+
+                    if botsys.wall_len()!=wall_buffer.get_num_verticies(){
+                        wall_buffer.re_generate_buffer(botsys.wall_len()); 
+                    }
+
+                    for (a,b) in wall_buffer.get_verts_mut().iter_mut().zip(botsys.wall_iter()){
+                        *a=Vertex([b.x,b.y,1.0]);
+                    }
+                    wall_buffer.update();
+
+                    
+                    
+                    if botsys.bot_len()!=bot_buffer.get_num_verticies(){
+                        bot_buffer.re_generate_buffer(botsys.bot_len()); 
+                    }
+                    for (a,b) in bot_buffer.get_verts_mut().iter_mut().zip(botsys.bots_iter()){
+                        let alpha=b.vel.magnitude2()*0.01;
+                        *a=Vertex([b.pos.x,b.pos.y,0.5+alpha])
+                    }
+                    bot_buffer.update();
+                    
+
+                    let mut ss=glsys.new_draw_session([0.0,0.0,0.0],border);
+                    ss.draw_vbo_section(&wall_buffer,0,botsys.wall_len(),[1.0,1.0,1.0],10.0,true);
+                    ss.draw_vbo_section(&bot_buffer,0,botsys.bot_len(),[1.0,1.0,1.0],2.0,false);
+                    ss.finish();
+        
+                    /*
+                    let mut ss=glsys.new_draw_session([0.0,0.0,0.0],border,radius,false);
+                    ss.draw_vbo_section(&bot_buffer,10,botsys.bot_len(),[1.0,0.0,1.0]);
+                    ss.finish();
+                    */
+
+                    last_time=Some(std::time::Instant::now());
+                }
+            },
+            _ => {},
+        }    
+    });
+
+    
+    
+}
