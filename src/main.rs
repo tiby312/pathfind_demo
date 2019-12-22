63cd9e22590bd8366bccedf4295e7ba3275dd4b3
Ken <kenakioreed@gmail.com>
2019-12-22 09:29:13 -0500

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use demodesktopgraphics::glutin;
-use demodesktopgraphics::GlSys;
-use demodesktopgraphics::circle_program;
-use demodesktopgraphics::vbo::*;
 
+use very_simple_2d::*;
+use very_simple_2d::glutin;
 
 use axgeom::Vec2;
 use axgeom::vec2;
@@ -13,51 +11,58 @@ use glutin::event::VirtualKeyCode;
 use glutin::event::Event;
 use glutin::event_loop::ControlFlow;
 
+/*
+use glutin::event::WindowEvent;
+
+use glutin::event::VirtualKeyCode;
+use glutin::event::Event;
+use glutin::event_loop::ControlFlow;
+*/
 
 
 //TODO make this a feature dependency.
-mod piston_debug;
+//mod piston_debug;
 
+/*
 fn main(){
     //default_main();
     piston_debug::piston_debug();
 }
+*/
 
-fn default_main() {
+
+fn main() {
     rayon::ThreadPoolBuilder::new().num_threads(num_cpus::get_physical()).build_global().unwrap();
      
-
     let events_loop = glutin::event_loop::EventLoop::new();
 
+    let (mut botsys,area)=pathfind::game::Game::new();
 
-    let mut botsys=pathfind::game::Game::new();
+    //let area= vec2(1920,1080);
 
-    let mut glsys=GlSys::new(&events_loop);
-    let mut circle_program=circle_program::CircleProgram::new();
+    let mut glsys=very_simple_2d::WindowedSystem::new(area.inner_as(),&events_loop);
+    //let mut glsys=very_simple_2d::FullScreenSystem::new(&events_loop);
+    //glsys.set_viewport_from_width(1920.);
 
-    let dim=glsys.get_dim();
-    println!("dim={:?}",dim);
+    let window_border:Vec2<f32>=glsys.get_dim().inner_as();
+    let aspect_ratio=axgeom::AspectRatio(window_border.inner_as());
+
+    //let symbols=Symbols::new();
+
     
-    //let mut border=compute_border(game_response.new_game_world.unwrap().0,[startx as f32,starty as f32]);
-    let border=axgeom::Rect::new(0.0,1920.,0.0,1080.);
-    
-    let _radius=10.0;
 
+    //let (mut botsys,game_response)=MenuGame::new(aspect_ratio,&symbols);
     
 
-    let mut bot_buffer=Buffer::create_vbo(0);
+    //let mut border=game_response.new_game_world.unwrap().0;
+    //glsys.set_viewport_from_width(border.width as f32);
+    //let radius=game_response.new_game_world.unwrap().1;
+    
+    //let mut color=game_response.color.unwrap();
 
-    let mut wall_buffer=Buffer::create_vbo(0);
-        
-
-    struct Ba{
-        pos:Vec2<f32>,
-        id:u64
-    }
 
     let mut mousepos=vec2(0.0,0.0);
     let mut mouse_active=false;
-    let mut poses:Vec<Ba>=Vec::new(); 
     
     let _fps=FPSCounter::new();
 
@@ -95,49 +100,6 @@ fn default_main() {
                         }
                     }
                 },
-                WindowEvent::Touch(touch)=>{
-                    let glutin::dpi::LogicalPosition{x,y}=touch.location;
-                    //let x=(x*0.84) as f32; //TODO why needed????
-                    //let y=(y*0.84) as f32; 
-                    let x=x as f32;
-                    let y=y as f32;
-
-                    match touch.phase{
-                        glutin::event::TouchPhase::Started=>{
-
-                            let mut found=false;
-                            for i in poses.iter(){
-                                if i.id == touch.id{
-                                    //panic!("There is a problem!");
-                                    found=true;
-                                    break;
-                                }
-                            }
-                            if found==false{
-                                poses.push(Ba{id:touch.id,pos:vec2(x,y)});
-                            }
-                        },
-                        glutin::event::TouchPhase::Ended | glutin::event::TouchPhase::Cancelled=>{
-                            //poses.clear();
-                            poses.retain(|a|a.id!=touch.id);
-                        },
-                        glutin::event::TouchPhase::Moved=>{
-                            let mut ok=false;
-                            for k in poses.iter_mut(){
-                                if k.id==touch.id{
-                                    k.pos=vec2(x,y);
-                                    ok=true;
-                                    break;
-                                }
-                            }
-                            
-                            if ok ==false{
-                                panic!("Didnt find touch");
-                            }
-                          
-                        }
-                    }
-                },
                 _=>{}
             },
             Event::EventsCleared=>{
@@ -156,79 +118,93 @@ fn default_main() {
 
                 if do_run{
 
-                    let mut va:Vec<Vec2<f32>>=poses.iter().map(|a|a.pos).collect();
                     if mouse_active{
-                        let mouseposx=mousepos.x-(dim.x as f32/2.0);
-                        let mouseposy=mousepos.y-(dim.y as f32/2.0);
-                    
-                        let ((x1,x2),(y1,y2))=border.get();
-                        let w=x2-x1;
-                        let h=y2-y1;
-
-                        let mouseposx=mouseposx*(w/dim.x as f32);
-                        let mouseposy=mouseposy*(h/dim.y as f32);
-                       
-                        va.push(vec2(mouseposx,mouseposy));
+                        /*
+                        let v=vec2(border.width as f32*(mousepos.x/window_border.x),
+                                   border.vec().y as f32*(mousepos.y/window_border.y));
+                   
+                        va.push(v);
+                        */
                     }
 
                     botsys.step();
+                    let (grid,walls) = botsys.get_wall_grid();
+                    let (bot_prop,bots)=botsys.get_bots();
 
+                    {
+                        let mut draw_session=glsys.get_sys();
 
-                    let wall_radius={
-                        let (grid,walls) = botsys.get_wall_grid();
+                        {
+                            let mut squares = draw_session.squares(grid.spacing*0.5,[1.0,1.0,1.0,0.5]);
 
-                        if walls.len()!=wall_buffer.get_num_verticies(){
-                            wall_buffer.re_generate_buffer(walls.len()); 
+                            for x in 0..walls.dim().x{
+                                for y in 0..walls.dim().y{
+                                    if walls.get(vec2(x,y)){
+                                        squares.add(grid.to_world_center(vec2(x,y)));
+                                    }
+                                }
+                            }
+                            squares.draw();
                         }
 
+                        {
+                            let mut lines = draw_session.lines(1.0,[1.0,0.0,0.0,0.3]);
+                            for b in bots.iter(){
 
-                        let ww=wall_buffer.get_verts_mut().iter_mut();
-
-                        for ((a,w),b) in walls.iter().zip(ww){
-                            let alpha=if w{
-                                1.0
-                            }else{
-                                0.2
-                            };
-                            let a=grid.to_world_center(a);
-                            //let a=a+grid.cell_radius()/2.0;
-
-                            *b=circle_program::Vertex([a.x,a.y,alpha]);
+                                if let pathfind::game::GridBotState::Moving(a,_b)=b.state{
+                                    let curr=a.pos();
+                                    let curr_pos=grid.to_world_center(curr);
+                                    lines.add(b.bot.pos,curr_pos);
+                                }
+                            }
+                            lines.draw();
                         }
+                        {
+                            let mut lines = draw_session.lines(1.0,[0.0,0.0,1.0,0.3]);
+                            for b in bots.iter(){
 
+                                if let pathfind::game::GridBotState::Moving(a,_b)=b.state{
+                            
+                                    if let Some(next)=a.peek(){
+                                        let next_pos=grid.to_world_center(next);
+                                        lines.add(b.bot.pos,next_pos);
+                                    }
+                                }
+                            }
+                            lines.draw();
+                        }
                         /*
-                        for (a,b) in wall_buffer.get_verts_mut().iter_mut().zip(botsys.wall_iter()){
-                            *a=Vertex([b.x,b.y,1.0]);
+                        for b in bots.iter(){
+                            let p=b.bot.pos.inner_as::<f64>();
+                            
+                            if let pathfind::game::GridBotState::Moving(a,_b)=b.state{
+                                let curr=a.pos();
+                                let curr_pos=grid.to_world_center(curr).inner_into::<f64>();
+                                line([1.0, 0.0, 0.0, 0.3], 1.0, [p.x,p.y, curr_pos.x, curr_pos.y], transform, g);
+
+                                let next=a.peek();
+                                if let Some(next)=a.peek(){
+                                    let next_pos=grid.to_world_center(next).inner_into::<f64>();
+                                    line([0.0, 0.0, 1.0, 0.3], 1.0, [p.x,p.y, next_pos.x, next_pos.y], transform, g);
+                                }
+
+                            }
+
+
+                            let r=bot_prop.radius.dis() as f64;
+                            let r=r*0.2;
+                            rectangle([1.0,0.0,1.0,2.0], [p.x-r,p.y-r,r*2.,r*2.], transform, g);
                         }
                         */
-                        wall_buffer.update();
 
-                        grid.cell_radius() 
-                    };
-                    
-                    
-                    if botsys.bot_len()!=bot_buffer.get_num_verticies(){
-                        bot_buffer.re_generate_buffer(botsys.bot_len()); 
+                        let mut circles = draw_session.circles(bot_prop.radius.dis()*0.2,[1.0,0.0,1.0,2.0]);
+                        for b in bots.iter(){
+                            circles.add(b.bot.pos);
+                        }
+                        circles.draw();
                     }
-                    let (bot_prop,bots)=botsys.get_bots();
-                    for (a,b) in bot_buffer.get_verts_mut().iter_mut().zip(bots.iter()){
-                        let b=&b.bot;
-                        let _alpha=b.vel.magnitude2()*0.01;
-                        *a=circle_program::Vertex([b.pos.x,b.pos.y,1.0])
-                    }
-                    bot_buffer.update();
-                    
-
-                    let mut ss=circle_program.new_draw_session([0.0,0.0,0.0],border);
-                    ss.draw_vbo_section(dim,&wall_buffer,0,botsys.get_wall_grid().1.len(),[1.0,0.0,1.0],wall_radius*0.9,false);
-                    ss.draw_vbo_section(dim,&bot_buffer,0,botsys.bot_len(),[1.0,1.0,0.0],bot_prop.radius.dis()*0.3,true);
                     glsys.swap_buffers();
-        
-                    /*
-                    let mut ss=glsys.new_draw_session([0.0,0.0,0.0],border,radius,false);
-                    ss.draw_vbo_section(&bot_buffer,10,botsys.bot_len(),[1.0,0.0,1.0]);
-                    ss.finish();
-                    */
+                    
 
                     last_time=Some(std::time::Instant::now());
                 }
